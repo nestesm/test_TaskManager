4ce6cecdc9ffd1552346e65ec9dd5115ccfbaf6a
Mariia Nesterenko <marynestes@gmail.com>
2024-11-30 00:32:55 +0400

    add task manger

--- task_manager.py
+++ task_manager.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 
-from task import Task
+from task import Task, Priority, Status
 from data_handler import DataHandler
 
 class TaskManager:
@@ -24,3 +24,35 @@ class TaskManager:
     def delete_task_by_id(self, task_id: int) -> None:
         self.tasks = [task for task in self.tasks if task.id != task_id]
         self.data_handler.save(self.tasks)
+        
+    def delete_task_by_category(self, category: str) -> None:
+        self.task = [task for task in self.tasks if task.category == category]
+        self.data_handler.save(self.tasks)
+    
+    def get_task(self, task_id: int) -> Optional[Task]:
+        return next((task for task in self.tasks if task.id == task_id), None)
+    
+    def get_tasks_by_category(self, category: str) -> List[Task]:
+        return [task for task in self.tasks if task.category == category]
+    
+    def update_task(self, task_id: int, **kwargs) -> bool:
+        task = self.get_task(task_id)
+        if not task:
+            return False
+        for key, value in kwargs:
+            if key in ["title", "description", "category", "due_date"]:
+                setattr(task, key, value)
+            elif key == "priority" and value in Priority.list_values():
+                task.priority = Priority(value)
+            elif key == "status" and value in Status.list_values():
+                task.status = Status(value)
+        self.data_handler.save(self.tasks)
+        return True
+    
+    def search_tasks(self, keyword: str = "", category: str = "", status: Optional[Status] = None ) -> List[Task]:
+        return [
+            task for task in self.tasks 
+            if (keyword.upper() in task.description.upper()) or (keyword.upper() in task.description.upper())
+            and (category.upper() == task.category.upper() if category else True)
+            and (status == task.status if status else True)
+        ]
